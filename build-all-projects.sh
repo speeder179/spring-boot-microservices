975a4a6f1dd1a864078094e891644068ffa9327b
Speeder179 <wangxuekai@chinacloud.com.cn>
2016-04-28 09:21:45 +0800

    add ignore list

--- build-all-projects.sh
+++ build-all-projects.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-cd api-gateway; ./gradlew clean build; cd ..
-cd auth-server; ./gradlew clean build; cd ..
-cd config-server; ./gradlew clean build; cd ..
-cd task-webservice; ./gradlew clean build; cd ..
-cd user-webservice; ./gradlew clean build; cd ..
-cd web-portal; ./gradlew clean build; cd ..
-cd webservice-registry; ./gradlew clean build; cd ..
-cd comments-webservice; ./gradlew clean build; cd ..
+cd api-gateway; gradle clean build; cd ..
+cd auth-server; gradle clean build; cd ..
+cd config-server; gradle clean build; cd ..
+cd task-webservice; gradle clean build; cd ..
+cd user-webservice; gradle clean build; cd ..
+cd web-portal; gradle clean build; cd ..
+cd webservice-registry; gradle clean build; cd ..
+cd comments-webservice; gradle clean build; cd ..
--- user-webservice/src/main/java/com/rohitghatol/microservices/user/Application.java
+++ user-webservice/src/main/java/com/rohitghatol/microservices/user/Application.java
@@ -6,6 +6,7 @@ package com.rohitghatol.microservices.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.security.oauth2.resource.EnableOAuth2Resource;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -38,6 +39,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan
 @EnableEurekaClient
 @EnableOAuth2Resource
+@EnableHystrix
 public class Application {
 	public static void main(String[] args) {
 
